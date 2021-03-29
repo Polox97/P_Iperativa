84ae5773ce0219676437a9e1cb944b7ce30af835
Polox97 <juanse.pulidob@gmail.com>
2021-03-28 19:22:43 -0500

    clases anteriores

---
+++ clase3.js
@@ -0,0 +1,27 @@
+
+function calcularIndiceDeMasaCorporal(peso,altura){
+    return peso/(altura*altura);
+}
+
+console.log(calcularIndiceDeMasaCorporal(60,1.72));
+
+function suma(num1, num2){
+    return num1 + num2;
+}
+
+function resta(num1, num2){
+    return num1 - num2;
+}
+
+function multiplicacion(num1, num2){
+    return num1 * num2;
+}
+
+function division(num1, num2){
+    return num1 / num2;
+}
+
+console.log(suma(3,4));
+console.log(resta(5,4));
+console.log(multiplicacion(3,4));
+console.log(division(20,4));
---
+++ prueba1.js
@@ -0,0 +1,7 @@
+var altura = "alto";
+var peso = 60;
+var viejo = true;
+
+console.log(altura);
+console.log(peso);
+console.log(viejo);
